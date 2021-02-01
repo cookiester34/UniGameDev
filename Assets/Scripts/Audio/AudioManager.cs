ffa93ee2f981fb0ab7155b8a880cb0ba49f9fa4f
CritneySpears <44713251+CritneySpears@users.noreply.github.com>
2021-02-01 15:29:47 +0000

    AudioManager implementation
    - Audio Manager Script
    - Audio Manager Prefab
    - Sound Script
    - Folders
    - Music asset for testing

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Loop through our list of sounds and add an audio source for each
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.soundClip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Music playback can be started here.
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " was not found. Check spelling!");
+            return;
+        }
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip soundClip;
+
+    [Range(0f, 2f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
