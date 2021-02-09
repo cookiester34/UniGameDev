560ed3f82c4f33aaa27e09522016e4112aebc88a
Ryan Skull <skuuully@gmail.com>
2021-02-09 11:53:37 +0000

    Tucked away UI into tabbable collapsible area

---
+++ Assets/Scripts/UI/UIBottomBar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UI {
+    /// <summary>
+    /// Component to hold all the UI bottom buttons that manages which ones are active at a given time.
+    /// </summary>
+    public class UIBottomBar : MonoBehaviour {
+        List<UIBottomBarButton> _buttons = new List<UIBottomBarButton>();
+        [SerializeField] private UIBottomBarButton beginActive;
+
+        private void Awake() {
+            _buttons = GetComponentsInChildren<UIBottomBarButton>().ToList();
+            ForceBeginActive();
+        }
+
+        /// <summary>
+        /// Forces the component into the state where the beginActive is active and the rest are not
+        /// </summary>
+        private void ForceBeginActive() {
+            if (beginActive != null) {
+                foreach (UIBottomBarButton button in _buttons) {
+                    button.ActivateContents(button == beginActive, true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Changes the active state of the buttons contents
+        /// </summary>
+        /// <param name="activeButton"></param>
+        public void MakeContentsActive(UIBottomBarButton activeButton) {
+            foreach (UIBottomBarButton button in _buttons) {
+                button.ActivateContents(button == activeButton);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIBottomBarButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI {
+    /// <summary>
+    /// Button which can control the active state of some game object
+    /// </summary>
+    public class UIBottomBarButton : MonoBehaviour {
+        /// <summary>
+        /// The contents which the button should modify the active state of
+        /// </summary>
+        [SerializeField] private GameObject content;
+
+        private void Awake() {
+            if (content == null) {
+                Debug.LogError("Bar component has not been set on the UI bottom bar button");
+            }
+        }
+
+        /// <summary>
+        /// Set the the buttons associated contents to the specified active state, if contents are set to active but it
+        /// is already active, it will instead hide the components if force is false
+        /// </summary>
+        /// <param name="active">The state to set the contents to</param>
+        /// <param name="force">Whether the active should be used as is, disallows hiding the contents if active is
+        /// called whilst already active</param>
+        public void ActivateContents(bool active, bool force = false) {
+            if (force) {
+                content.SetActive(active);
+            } else {
+                if (content.activeSelf) {
+                    content.SetActive(false);
+                } else {
+                    content.SetActive(active);
+                }
+            }
+        }
+    }
+}
