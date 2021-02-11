5391f433f0aeb77aae4a286539b595180463fb0e
CritneySpears <44713251+CritneySpears@users.noreply.github.com>
2021-02-11 13:27:57 +0000

    Music queuing system & music lists
    Music Queue class
    Combat & peaceful music added
    Music assigned in audio manager

---
+++ Assets/Scripts/Audio/Music.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Music
+{
+    public string name;
+
+    public AudioClip musicClip;
+
+    [Range(0f, 2f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    /* Not Yet Implemented
+    public bool fadeOut;
+    */
+
+    public AudioMixerGroup output;
+
+    [HideInInspector]
+    public AudioSource source;
+}
