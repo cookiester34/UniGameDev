8ade46cd1bd991e36e47c8479e9d6640bd7edd00
Ryan Skull <skuuully@gmail.com>
2021-02-15 11:49:12 +0000

    Added health component with UI

---
+++ Assets/Scripts/SaveLoad/SaveWrappers/SavedHealth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SavedHealth {
+    public float currentHealth;
+    public float maxHealth;
+    
+    public SavedHealth(Health health) {
+        currentHealth = health.CurrentHealth;
+        maxHealth = health.MaxHealth;
+    }
+}
--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,16 @@ public class ProgressBar : MonoBehaviour {
     /// Slider component
     /// </summary>
     private Slider _slider;
+
+    /// <summary>
+    /// Fill component of the progress bar
+    /// </summary>
+    [SerializeField] private Image fill;
+    
+    /// <summary>
+    /// Potential colours to use for the slider
+    /// </summary>
+    private List<ColourThreshold> _colourThresholds;
 
     /// <summary>
     /// The target for the progress, allows the progress to smoothly increment
@@ -26,23 +37,90 @@ public class ProgressBar : MonoBehaviour {
         set => _targetProgress = value;
     }
 
+    public List<ColourThreshold> ColourThresholds {
+        set {
+            _colourThresholds = value;
+            _colourThresholds.Sort((threshold1, threshold2) => 
+                (int) ((threshold1.threshold - threshold2.threshold) * 10f));
+        }
+    }
+
     private void Awake() {
         _slider = GetComponent<Slider>();
         if (_slider == null) {
             Debug.LogError("Progress bar is missing its slider component");
         }
 
+        if (fill == null) {
+            Debug.LogError("Progress bar is missing its fill component");
+        }
+
+        _slider.interactable = false;
         _slider.value = 0;
         _targetProgress = 0;
     }
 
     /// <summary>
-    /// Smoothly move the progess towards the target
+    /// Smoothly move the progress towards the target
     /// </summary>
     private void Update() {
-        if (_slider.value < _targetProgress) {
-            _slider.value += fillSpeed * Time.deltaTime;
-            _slider.value = Mathf.Clamp(_slider.value, 0, _targetProgress);
+
+        if ((_slider.value < _targetProgress) || (_slider.value > _targetProgress)) {
+            var currentValue = _slider.value;
+
+            // Fill direction used to determine whether the value should increase or decrease 
+            float fillDirection = currentValue < _targetProgress ? 1f: -1f;
+            float smoothValue = currentValue + (fillSpeed * Time.deltaTime * fillDirection);
+
+            SetSliderValue(Mathf.Clamp(smoothValue, 0, _targetProgress));
         }
+    }
+
+    /// <summary>
+    /// Changes the value of the slider 
+    /// </summary>
+    /// <param name="value">A value between 0 and 1</param>
+    /// <param name="instant">Whether it should instantly happen causing the target progress to update</param>
+    public void SetSliderValue(float value, bool instant = false) {
+        if (instant) {
+            _targetProgress = value;
+        }
+
+        _slider.value = Mathf.Clamp(value, 0, 1);
+        UpdateColour();
+    }
+    
+    /// <summary>
+    /// Updates the colour of the fill component if colour thresholds have been supplied
+    /// </summary>
+    private void UpdateColour() {
+        if (_colourThresholds != null) {
+            foreach (ColourThreshold colourThreshold in _colourThresholds) {
+                if (_slider.value < colourThreshold.threshold) {
+                    fill.color = colourThreshold.color;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Util so that colours can be tied to a value
+/// </summary>
+public class ColourThreshold {
+    /// <summary>
+    /// The colour paired to the threshold
+    /// </summary>
+    public Color color;
+
+    /// <summary>
+    /// Threshold between 0 and 1 where the colour should be used
+    /// </summary>
+    public float threshold;
+
+    public ColourThreshold(Color color, float threshold) {
+        this.color = color;
+        this.threshold = threshold;
     }
 }
