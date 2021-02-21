a8361fbb1fc9264fc919789346f68c2d1b5d39d2
Peter <pdowen24@gmail.com>
2021-02-21 13:39:43 +0000

    Basic pathfinding and dynamic navmesh

---
+++ Assets/Scripts/Pathfinding/NavMeshDynamic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshDynamic : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshSurface[] navMeshSurfaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < navMeshSurfaces.Length; i++)
+        {
+            navMeshSurfaces[i].BuildNavMesh();
+        }
+    }
+
+   
+}
