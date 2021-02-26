7362e1e30c42edcc009dd066a859b7e366fc1fcf
GeneralUKGinge <jameshyde0897@hotmail.com>
2021-02-26 11:03:25 +0000

    Art VFX added

---
+++ Assets/VArtFX/Scripts/Snow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour {
+
+    [Range(0.01f, 0.1f)]
+    public float snowCoverSpeed;
+    float value = 0;
+
+    private void Update() {
+        
+        if (value < 1f) {
+
+            Shader.SetGlobalFloat("_SnowLevel", value);
+            value += Time.deltaTime * snowCoverSpeed;
+
+        }
+    }
+
+}
