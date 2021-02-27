b8c0cd512492c075ded6574d92ce35c668b4e7af
Ryan Skull <skuuully@gmail.com>
2021-02-27 16:17:00 +0000

    Fix for:Some objects were not cleaned up when closing the scene

--- Assets/Scripts/Util/ApplicationUtil.cs
+++ Assets/Scripts/Util/ApplicationUtil.cs
@@ -6,13 +6,14 @@ public static class ApplicationUtil {
     /// <summary>
     /// Used to check if the application is currently quitting, strangely not already supported by application
     /// </summary>
-    private static bool _isQuitting;
+    private static bool _isQuitting = false;
     public static bool IsQuitting => _isQuitting;
     
     static ApplicationUtil() {
-        _isQuitting = false;
+        Debug.Log("Application util constructed");
         Application.quitting += () => {
             _isQuitting = true;
+            Debug.Log("Quitting");
         };
     }
 }
---
+++ Assets/Scripts/Util/QuittingObj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Just makes sure the static applicationUtil is constructed
+/// </summary>
+public class QuittingObj : MonoBehaviour {
+    private void Awake() {
+        if (ApplicationUtil.IsQuitting) { }
+    }
+}
