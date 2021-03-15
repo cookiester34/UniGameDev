bebecde0795118015166f2c9de3ed904e2a44d43
Skuuully <skuully@gmail.com>
2021-03-15 19:54:57 +0000

    Saving no longer saves the grid just the status of whether a tile can be placed on

---
+++ Assets/Scripts/FogOfWar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour {
+    [SerializeField] private GameObject fogOfWar;
+    
+    private void Awake() {
+        fogOfWar.SetActive(CurrentSceneType.SceneType == SceneType.GameLevel);
+    }
+}
