6e6132fe69bfc0901a3b7c51ce99e561a0cdcac9
Skuuully <skuully@gmail.com>
2021-03-18 09:52:48 +0000

    Small change to allow build to work, generated weeks build

--- Assets/Scripts/Util/HexHighlighter.cs
+++ Assets/Scripts/Util/HexHighlighter.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Quick attempt at highlighting hexes near mouse when in build mode
