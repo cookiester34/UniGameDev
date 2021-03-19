f3e223e03af62f0a79d69546b6c8357bf83e627d
Skuuully <skuully@gmail.com>
2021-03-19 11:23:02 +0000

    Removed a whole bunch of stuff/moved some stuff

---
+++ Assets/Scripts/UI/RadialProgress.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class RadialProgress : MonoBehaviour
+{
+    public Image progressBar;
+    float currentValue;
+    public float fillSpeed;
+    public bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            if (currentValue < 1f)
+            {
+                currentValue += Time.deltaTime * fillSpeed;
+            }
+            else
+            {
+                currentValue = 0;
+            }
+            progressBar.fillAmount = currentValue;
+        }
+    }
+}
