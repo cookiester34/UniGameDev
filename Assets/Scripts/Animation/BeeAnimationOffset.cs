488f09e94cabcf584091a166778da3a9fd3a877e
Skuuully <skuully@gmail.com>
2021-04-16 11:12:32 +0100

    Uses new bee ((currently untextured)

---
+++ Assets/Scripts/Animation/BeeAnimationOffset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BeeAnimationOffset : MonoBehaviour {
+    private Animator _animator;
+    private static readonly int FlyOffset = Animator.StringToHash("FlyOffset");
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Start() {
+        _animator.SetFloat(FlyOffset, Random.Range(0f, 1f));
+    }
+}
