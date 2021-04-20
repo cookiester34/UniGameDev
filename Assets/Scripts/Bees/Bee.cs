33a7fd92c0e48c6a574a6f5333b8024a85b57155
cookie <cookiejoecook@outlook.com>
2021-04-20 13:33:51 +0100

    updated all ai, fixed bug where ai would fall through ground

--- Assets/Scripts/Bees/Bee.cs
+++ Assets/Scripts/Bees/Bee.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+//using UnityEngine.AI;
+using Pathfinding;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(BeeStateMachine))]
-[RequireComponent(typeof(NavMeshAgent))]
+//[RequireComponent(typeof(NavMeshAgent))]
 public class Bee : MonoBehaviour {
     private Building _home;
     private Building _work;
     private BeeStateMachine _stateMachine;
-    private NavMeshAgent _agent;
+    //private NavMeshAgent _agent;
+    private IAstarAI _agent;
 
 
     private float _soundLoopBasePitch;
@@ -35,7 +37,7 @@ public class Bee : MonoBehaviour {
         set => _work = value;
     }
 
-    public NavMeshAgent Agent {
+    public IAstarAI Agent {
         get => _agent;
         set => _agent = value;
     }
@@ -44,7 +46,7 @@ public class Bee : MonoBehaviour {
         idCounter++;
         id = idCounter;
 
-        _agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<IAstarAI>();
 
         _soundLoopBasePitch = 1f + UnityEngine.Random.Range(-.05f, 0.05f);
         _soundLoopVolume = 1f + UnityEngine.Random.Range(-.05f, 0.05f);
@@ -52,7 +54,7 @@ public class Bee : MonoBehaviour {
         transform.GetComponent<AudioSource>().pitch = _soundLoopBasePitch;
 
         _agent.height = Random.Range(0.2f, 2f);
-        _agent.baseOffset = _agent.height / 2f;
+        //_agent.baseOffset = _agent.height / 2f;
 
         var scale = Random.Range(0.6f, 0.9f);
         transform.localScale = new Vector3(scale, scale, scale);
@@ -62,6 +64,15 @@ public class Bee : MonoBehaviour {
         _home = GetValidHouse();
         if (_home != null) {
             _home.AssignBee(this);
+        }
+    }
+
+    private void Update()
+    {
+        if(transform.position.y < 0)
+        {
+            Vector3 resetHeight = new Vector3(transform.position.x, 1, transform.position.z);
+            transform.position = resetHeight;
         }
     }
 
