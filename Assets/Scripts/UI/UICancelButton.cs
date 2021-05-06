389a19f9a5e1873846b7ad45ed8f30d0be30db1a
Skuuully <skuully@gmail.com>
2021-05-06 12:10:34 +0100

    The cancel button for menus works now

--- Assets/Scripts/UI/UICancelButton.cs
+++ Assets/Scripts/UI/UICancelButton.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// A button that can be used as a cancel button to hide the menu
+/// </summary>
 [RequireComponent(typeof(Button))]
 public class UICancelButton : MonoBehaviour {
+    /// <summary>
+    /// The menu that should be cancelled when the button is clicked
+    /// </summary>
+    [Tooltip("The menu that should be cancelled when the cancel button is clicked")]
     [SerializeField] private GameObject cancelObject;
     private Button _button;
     
     private void Awake() {
         _button = GetComponent<Button>();
+
+        if (cancelObject == null) {
+            Debug.LogWarning("Cancel button missing an object to cancel when it is clicked");
+        }
     }
 
-
-    void CancelUI() {
-        
+    private void OnEnable() {
+        _button.onClick.AddListener(CancelUi);
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    private void OnDisable() {
+        _button.onClick.RemoveListener(CancelUi);
+    }
+
+    void CancelUi() {
+        cancelObject.SetActive(false);
     }
 }
