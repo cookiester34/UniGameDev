ea53b1edcff395352374e7eab02662e25c805539
Skuuully <skuully@gmail.com>
2021-05-09 16:09:10 +0100

    Fixed the selected building tab reselecting itself, new tabbable stuff introduced for it

---
+++ Assets/Scripts/UI/Tabbable/TabbableCollection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A collection that holds multiple tabbable contents, ensures only one is active at a time
+/// </summary>
+public class TabbableCollection : MonoBehaviour {
+    [SerializeField] private TabbableContent[] _contents;
+    [SerializeField] private TabbableContent beginningActiveContent;
+    private TabbableContent activeContent = null;
+
+    public TabbableContent ActiveContent {
+        set => activeContent = value;
+    }
+
+    private void Start() {
+        foreach (TabbableContent content in _contents) {
+            content.Hide();
+        }
+        
+        if (beginningActiveContent != null) {
+            MakeActive(beginningActiveContent);
+        }
+    }
+
+    public void MakeActive(TabbableContent content) {
+        if (activeContent != null) {
+            activeContent.gameObject.SetActive(false);
+        }
+        content.gameObject.SetActive(content != activeContent);
+    }
+
+    public void Deactivate(TabbableContent content) {
+        content.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/Tabbable/TabbableContent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Belongs as part of a tabbable collection, and has a button that enables its content
+/// </summary>
+public class TabbableContent : MonoBehaviour {
+    [Tooltip("The collection this content belongs to")]
+    [SerializeField] private TabbableCollection collection;
+    
+    [Tooltip("Whether this contents button should be hidden when this content is")]
+    [SerializeField] private bool hideButtonOnDismiss;
+    
+    [Tooltip("The button to show this content")]
+    [SerializeField] private Button associatedButton;
+    
+    public event Action OnHide;
+
+    private void Awake() {
+        
+        if (collection == null) {
+            Debug.LogWarning("TabbableContent misused, requires a collection to belong to");
+        }
+        
+        if (associatedButton == null) {
+            Debug.LogWarning("TabbableContent misused, requires a button to control its visibility");
+        }
+    }
+
+    public void Toggle() {
+        if (gameObject.activeSelf) {
+            Hide();
+        } else {
+            Show();
+        }
+    }
+
+    public void Hide() {
+        collection.Deactivate(this);
+        if (hideButtonOnDismiss) {
+            associatedButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void Show() {
+        collection.MakeActive(this);
+    }
+
+    private void OnEnable() {
+        collection.ActiveContent = this;
+        if (hideButtonOnDismiss) {
+            associatedButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDisable() {
+        collection.ActiveContent = null;
+        if (hideButtonOnDismiss) {
+            associatedButton.gameObject.SetActive(false);
+        }
+        OnHide?.Invoke();
+    }
+}
