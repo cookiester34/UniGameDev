d568aa85bbe43b2afdc781a821765d00b4c4ac2d
ComputerBot2016 <thz343@yahoo.co.uk>
2021-05-10 14:31:38 +0100

    Season jumping now works with season scroller
    Also fixed bug where changing the season duration would not apply until the following season

--- Assets/Scripts/UI/SeasonProgressBar.cs
+++ Assets/Scripts/UI/SeasonProgressBar.cs
@@ -9,6 +9,7 @@ public class SeasonProgressBar : MonoBehaviour
     private float _halfWidth;
     private float _posX = 0f;
     public float singleSeasonWidth = 278f;
+    private float _halfSeasonWidth;
     public bool isDebug = false;
     public float dbgScrollSpeed = 1f;
     private float _scrollSpeed;
@@ -22,9 +23,11 @@ public class SeasonProgressBar : MonoBehaviour
         } else
         {
             _scrollSpeed = singleSeasonWidth / (float)SeasonManager.Instance.seasonLength;
+            SeasonManager.SeasonChange += HandleSeasonChange;
         }
         _rt = GetComponent<RectTransform>();
         _halfWidth = (_rt.sizeDelta.x / 2f) * _rt.localScale.x;
+        _halfSeasonWidth = singleSeasonWidth / 2f;
         _posX = _rt.anchoredPosition.x;
         _rt.anchoredPosition = new Vector2(_posX, _rt.anchoredPosition.y);
     }
@@ -33,6 +36,38 @@ public class SeasonProgressBar : MonoBehaviour
     void Update()
     {
         _posX -= _scrollSpeed * Time.deltaTime;
+        UpdatePosition();
+    }
+
+    void HandleSeasonChange()
+    {
+        //Debug.Log(SeasonManager.Instance.GetCurrentSeason().ToString());
+        Seasons currentSeason = SeasonManager.Instance.GetCurrentSeason();
+        switch (currentSeason)
+        {
+            case Seasons.Spring:
+                ForceScrollToPosition(-((singleSeasonWidth * 4f) - _halfSeasonWidth));
+                break;
+            case Seasons.Summer:
+                ForceScrollToPosition(-(singleSeasonWidth - _halfSeasonWidth));
+                break;
+            case Seasons.Autumn:
+                ForceScrollToPosition(-((singleSeasonWidth * 2f) - _halfSeasonWidth));
+                break;
+            case Seasons.Winter:
+                ForceScrollToPosition(-((singleSeasonWidth * 3f) - _halfSeasonWidth));
+                break;
+        }
+    }
+
+    void ForceScrollToPosition(float pos)
+    {
+        _posX = pos;
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
         if (_posX <= -_halfWidth)
         {
             _posX += _halfWidth;
