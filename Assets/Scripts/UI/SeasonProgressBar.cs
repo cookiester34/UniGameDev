475faf4afe63043ab4ae166e6fb30a7797b6822e
ComputerBot2016 <thz343@yahoo.co.uk>
2021-05-10 12:05:30 +0100

    Basic scrolling season UI (very WIP)

--- Assets/Scripts/UI/SeasonProgressBar.cs
+++ Assets/Scripts/UI/SeasonProgressBar.cs
@@ -8,20 +8,30 @@ public class SeasonProgressBar : MonoBehaviour
     private RectTransform _rt;
     private float _halfWidth;
     private float _posX = 0f;
+    public bool isDebug = false;
     public float dbgScrollSpeed = 1f;
+    private float _scrollSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (isDebug)
+        {
+            _scrollSpeed = dbgScrollSpeed;
+        } else
+        {
+            _scrollSpeed = 100f / (float)SeasonManager.Instance.seasonLength;
+        }
         _rt = GetComponent<RectTransform>();
         _halfWidth = _rt.sizeDelta.x / 2f;
+        _posX = _rt.anchoredPosition.x;
         _rt.anchoredPosition = new Vector2(_posX, _rt.anchoredPosition.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _posX -= dbgScrollSpeed * Time.deltaTime;
+        _posX -= _scrollSpeed * Time.deltaTime;
         if (_posX <= -_halfWidth)
         {
             _posX += _halfWidth;
