be99f522dc10e8ca97cd4433743b475174429126
cookie <cookiejoecook@outlook.com>
2021-05-16 11:53:29 +0100

    added wait for upgrade button

---
+++ Assets/Scripts/UI/ButtonInteractableTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteractableTimer : MonoBehaviour
+{
+    private Button _button;
+    private bool disableButton = true;
+    private bool Toggle = false;
+    public void OnClick(Button button)
+    {
+        _button = button;
+        StartCoroutine("ToggleButton");
+    }
+
+    private void Update()
+    {
+        if (Toggle)
+        {
+            _button.interactable = disableButton;
+        }
+    }
+
+    IEnumerator ToggleButton()
+    {
+        disableButton = false;
+        Toggle = true;
+        yield return new WaitForSecondsRealtime(0.5f);
+        disableButton = true;
+        yield return new WaitForEndOfFrame();
+        Toggle = false;
+    }
+}
